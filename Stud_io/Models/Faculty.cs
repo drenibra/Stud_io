d49c564f7286fd2a8f25cd3f42022d6586f02d7c
dren <drenibrahimi02@gmail.com>
2023-04-21 18:13:41 +0200

    Added Authentication, Authorization, Roles, Student Class

---
+++ Stud_io/Models/Faculty.cs
@@ -0,0 +1,9 @@
+﻿namespace Stud_io.Models
+{
+    public class Faculty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Major> Majors {  get; set; }
+    }
+}
