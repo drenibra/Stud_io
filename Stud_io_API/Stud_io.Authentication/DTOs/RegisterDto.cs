970440d95b8c94a81a8a66d4427e90afe56d4ace
dren <drenibrahimi02@gmail.com>
2023-04-23 03:13:29 +0200

    Added Authentication, Authorization, JWT Token, Roles, Student Class

---
+++ Stud_io_API/Stud_io.Authentication/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Stud_io.DTOs
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ Stud_io_API/Stud_io.Authentication/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Stud_io.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+    }
+}
