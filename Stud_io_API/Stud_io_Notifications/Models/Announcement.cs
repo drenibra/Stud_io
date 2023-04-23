d0c8f0404306603b13247395ad8ddba6a3e13931
Alma <almanovoberdaliu@gmail.com>
2023-04-23 22:19:22 +0200

    Added appsettings.json files

--- Stud_io_API/Stud_io_Notifications/Models/Announcement.cs
+++ Stud_io_API/Stud_io_Notifications/Models/Announcement.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public int DeadlineId { get; set; }
+        public Deadline Deadline { get; set; }
     }
 }
