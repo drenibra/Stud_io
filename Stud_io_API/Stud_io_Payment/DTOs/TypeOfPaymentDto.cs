b2e1f14afbb40da5cb56c650bc67ee6ab4abba9e
Fat Sijarina <fs51701@ubt-uni.net>
2023-04-26 11:29:23 +0200

    Completed crud operations of TypeOfPayment model, included sorting and filtering

--- Stud_io_API/Stud_io_Payment/DTOs/TypeOfPaymentDto.cs
+++ Stud_io_API/Stud_io_Payment/DTOs/TypeOfPaymentDto.cs
@@ -2,6 +2,13 @@
 {
     public class TypeOfPaymentDto
     {
+        public string Type { get; set; } = null!;
+        public double Price { get; set; }
+    }
 
+    public class UpdateTypeOfPaymentDto
+    {
+        public string? Type { get; set; }
+        public double? Price { get; set; }
     }
 }
