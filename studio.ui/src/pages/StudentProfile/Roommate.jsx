ec328abf502815281b3f2a450c0b05a43ec09b93
BleonaGerbavci <bg52732@ubt-uni.net>
2023-06-02 20:22:07 -0700

    finished Roommate page in front-end

--- studio.ui/src/pages/StudentProfile/Roommate.jsx
+++ studio.ui/src/pages/StudentProfile/Roommate.jsx
@@ -1,12 +1,12 @@
 import Menu from "../../components/Menu/Menu";
 import "./roommate.scss";
 import { TextField } from "@mui/material";
-import { Avatar, IconButton } from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from '@mui/material';
+import Button from "@mui/material/Button";
 import accept from "../StudentProfile/img/accept3.svg"
 import reject from "../StudentProfile/img/reject.svg"
 import profilep from "../StudentProfile/img/profilep.svg"
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 
 export default function Roommate() {
 return(
@@ -49,13 +49,56 @@ return(
                     </div>
                 </div>
         </div>
-        <div className="secondPart">
-            <h2></h2>
-            <div className="ChooseRoommate">
-                
+        <div className="secondPart"> 
+            <div className="chooseRoommate">
+                <div className="tit4">
+                    <h4>Zgjedh shokun e dhomës</h4> 
+                </div>
+                <p>Shëno të dhënat e shokut:</p>
+                    <div className="Textfields"> 
+                        <TextField label="Emri" value=""  size= "small"  sx={{ mb: 2, mt: 3.5}} />
+                        <TextField label="Mbiemri" value=""  size= "small"  sx={{ mb: 2 }} />   
+                    </div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        small
+                        style={{ borderRadius: '30px', textTransform: 'none' }}
+                    >
+                        Kërko
+                    </Button>
             </div>
-            <div className="RoommateSuggestion">
-                    
+            <div className="roommateSuggestion">
+                <h4>Merr sugjerim nga sistemi</h4> 
+
+                <div className="questionnaire">
+                <span>Plotësoni pyetësorin</span>
+                <IconButton style={{ color: '#bf1a2f', borderRadius: '5px', padding: '5px' }}>
+                    <AssignmentTurnedInIcon style={{ fontSize: '80px' }} />
+                </IconButton>
+                </div>
+                
+                <div className="pAndContainer">
+                    <p>Shoku i sugjeruar:</p> 
+                    <div className="roommateRequestContainer">
+                        <div className="img">
+                            <img src={profilep}></img>
+                        </div>
+                        <p>Filan Fisteku</p>
+                        <div className="iconContainer">
+                            <div className="greenBackground">
+                                <IconButton>
+                                    <img src={accept} alt="Accept" />
+                                </IconButton>
+                            </div>
+                            <div className="redBackground">
+                                <IconButton>
+                                    <img src={reject} alt="Reject" />
+                                </IconButton>
+                            </div>       
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
