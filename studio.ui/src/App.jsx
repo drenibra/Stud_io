560a06131775ba0308daae421fa3685447c3110f
Rrezart Hetemi <rrezart.hetemi@outlook.com>
2023-06-06 01:07:26 +0200

    Update App.jsx

--- studio.ui/src/App.jsx
+++ studio.ui/src/App.jsx
@@ -1,42 +1,13 @@
-import { useEffect, useState } from "react";
-import agent from "./api/account_agent";
+import { useState } from "react";
 import "./App.scss";
 import ResponsiveAppBar from "./components/Navbar/ResponsiveAppBar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/footer";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./styles/theme";
 import { Routes, Route } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { useStore } from "./stores/store";
-<<<<<<< HEAD
-import LoginForm from "./pages/Authentication/Loginform";
-import Apply from "./pages/Application/Apply";
-import RegisterForm from "./pages/Authentication/RegisterForm";
-import ProtectedRoute from "./ProtectedRoute";
-
-import {
-  LandingPage,
-  Maintenance,
-  Payment,
-  Payments,
-  MyProfile,
-  RegisterCustomer,
-  Announcement,
-  Roommate,
-  Questionnaire,
-} from "./pages";
-
-const routes = [
-  { path: "/maintenance", element: Maintenance },
-  { path: "/payment", element: Payment },
-  { path: "/payments", element: Payments },
-  { path: "/myprofile", element: MyProfile },
-  { path: "/registerCustomer", element: RegisterCustomer },
-  { path: "/announcement", element: Announcement },
-  { path: "/roomate", element: Roommate },
-  { path: "/questionnaire", element: Questionnaire },
-];
-=======
 import LoginForm from "./pages/Login/LoginForm";
 
 import {
@@ -54,52 +25,10 @@ import {
 
 const App = observer(function App() {
   const { userStore } = useStore();
->>>>>>> maintenance
 
-const App = observer(function App() {
-  const { commonStore, userStore } = useStore();
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    if (commonStore.token) {
-      try {
-        const fetchData = async () => {
-          const userData = await agent.Account.current();
-          setUser(userData);
-        };
-        fetchData();
-      } catch (error) {
-        console.error(error);
-      } finally {
-        commonStore.setAppLoaded();
-      }
-    } else {
-      commonStore.setAppLoaded();
-    }
-  }, [commonStore, userStore]);
-
-  return (
+  return userStore.isLoggedIn ? (
     <ThemeProvider theme={theme}>
       <ResponsiveAppBar />
-<<<<<<< HEAD
-      <Routes>
-        {routes.map((route) => (
-          <Route
-            path={route.path}
-            key={route.key}
-            element={
-              <ProtectedRoute loggedIn={userStore.isLoggedIn}>
-                <route.element />
-              </ProtectedRoute>
-            }
-          />
-        ))}
-        <Route element={<LoginForm />} path="/login" />
-        <Route element={<RegisterForm />} path="/register" />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<LandingPage />} />
-      </Routes>
-=======
       <div className="main-container">
         <Routes>
           <Route path="/" element={<LandingPage />} />
@@ -114,9 +43,13 @@ const App = observer(function App() {
           <Route path="/Questionnaire" element={<Questionnaire />} />
         </Routes>
       </div>
->>>>>>> maintenance
       <Footer />
     </ThemeProvider>
+  ) : (
+    <>
+      {console.log(import.meta.env.VITE_REACT_APP_API_AUTH_URL)}
+      <LoginForm />
+    </>
   );
 });
 
