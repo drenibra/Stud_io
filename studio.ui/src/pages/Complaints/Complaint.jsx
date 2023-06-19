7d30b0cb78618780950d69952cbd4e503646da10
almanovoberdaliu <93130020+almanovoberdaliu@users.noreply.github.com>
2023-06-19 03:40:37 +0200

    Added complaint page in frontend

--- studio.ui/src/pages/Complaints/Complaint.jsx
+++ studio.ui/src/pages/Complaints/Complaint.jsx
@@ -1,8 +1,57 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useStore } from '../../stores/store';
+import { ToastContainer, toast } from 'react-toastify';
+import './styles.scss';
+import TextField from '@mui/material/TextField';
+
 export default function Complaint()
 {
+    const { userStore } = useStore();
+    let studentId = userStore.user.id;
+    const [complaintForm, setComplaintForm] = useState({
+        description: '',
+        studentsId: studentId
+    });
+
+    const handleChange = (e) =>
+    {
+        const newData = { ...complaintForm };
+        newData[e.target.id] = e.target.value;
+        setComplaintForm(newData);
+    };
+
+    const handleSubmit = async (e) =>
+    {
+        e.preventDefault();
+        try
+        {
+            const response = await axios.post('https://localhost:7007/AddComplaint', complaintForm);
+            console.log(response.data);
+            toast.success("Ankesa u ruajt me sukses!");
+        } catch (error)
+        {
+            toast.error('An error occurred while adding the complaint.');
+        }
+    }
+
     return (
         <div>
-            <h3>Hello</h3>
+            <h2 className='h2-ankesat'>Paraqisni një ankesë</h2>
+            <form className="form-container-ankesat" onSubmit={handleSubmit}>
+                <textarea
+                    id="description"
+                    className="textarea-field-ankesat"
+                    value={complaintForm.description}
+                    onChange={handleChange}
+                    placeholder="Shkruani ankesen tuaj këtu..."
+                    required
+                />
+                <button type="submit" className="submit-button-ankesat">
+                    Submit
+                </button>
+            </form>
+            <ToastContainer />
         </div>
     )
 }
