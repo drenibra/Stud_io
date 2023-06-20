270c37a5d55b3e53ace6038a9cfbe4871378e3aa
drenibra <drenibrahimi02@gmail.com>
2023-06-20 23:58:29 +0200

    dormitories styles

--- studio.ui/src/pages/PrincipalDashboard/Dormitories.jsx
+++ studio.ui/src/pages/PrincipalDashboard/Dormitories.jsx
@@ -14,12 +14,16 @@ import { ToastContainer, toast } from 'react-toastify';
 import Slide from '@material-ui/core/Slide';
 import Grow from '@material-ui/core/Grow';
 import { Link } from 'react-router-dom';
+import { useStore } from '../../stores/store';
+import './dormitory.css';
 
 export default function Dormitories() {
   const [dormitories, setDormitories] = useState([]);
   const [refreshKey, setRefreshKey] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [profileMatches, setProfileMatches] = useState([]);
+
+  const { userStore } = useStore();
 
   const updateDormitoryData = async () => {
     try {
@@ -72,71 +76,73 @@ export default function Dormitories() {
         </Box>
       )}
 
-      <Grid container justifyContent="center" marginTop={4}>
-        {isLoading ? (
-          <CircularProgress color="secondary" />
-        ) : (
-          <Button variant="contained" color="secondary" onClick={updateDormitoryData} style={{ backgroundColor: '#bf1a2f', borderRadius: '10px' }}>
-            Cakto studentët në konvikte
-          </Button>
-        )}
-      </Grid>
-      <Grid container justifyContent="center">
-        <Grid item xs={12} md={10} lg={8}>
-          <Container style={{ padding: '30px' }}>
-            <Grid container spacing={2}>
-              {dormitories.map((dormitory, index) => (
-                <Grid item key={index} xs={12} sm={6} md={3}>
-                  <Slide direction="up" in timeout={300}>
-                    <Link to="/dormitory" style={{ textDecoration: 'none' }}>
-                      <Card
-                        style={{
-                          backgroundColor: '#f3f3f3',
-                          boxShadow: '0px 0px 10px rgba(148, 9, 9, 0.479)',
-                          borderRadius: '10px',
-                          transition: 'width 250ms ease-in-out, transform 150ms ease',
-                          '&:hover': {
-                            cursor: 'pointer',
-                          },
-                        }}
-                      >
-                        <Grow in timeout={300}>
-                          <CardContent>
-                            <Container
-                              style={{
-                                backgroundColor: '#bf1a2f',
-                                padding: '8px',
-                                marginBottom: '8px',
-                                borderRadius: '10px',
-                              }}
-                            >
-                              <Typography variant="h6" gutterBottom style={{ fontWeight: 'bold', color: '#ffffff' }}>
-                                Konvikti {dormitory.dormNo}
-                              </Typography>
-                            </Container>
-                            <Typography variant="body2" color="textSecondary">
-                              Gjinia: {dormitory.gender}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                              Numri i dhomave: {dormitory.noOfRooms}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                              Kapaciteti: {dormitory.capacity}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                              Studentët aktual: {dormitory.currentStudents}
-                            </Typography>
-                          </CardContent>
-                        </Grow>
-                      </Card>
-                    </Link>
-                  </Slide>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
+      <div className="dormitory-main">
+        <Grid container justifyContent="center" marginTop={4}>
+          {isLoading ? (
+            <CircularProgress color="secondary" />
+          ) : (
+            <Button variant="contained" color="secondary" onClick={updateDormitoryData} style={{ backgroundColor: '#bf1a2f', borderRadius: '10px' }}>
+              Cakto studentët në konvikte
+            </Button>
+          )}
         </Grid>
-      </Grid>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} md={10} lg={8}>
+            <Container style={{ padding: '30px' }}>
+              <Grid container spacing={2}>
+                {dormitories.map((dormitory, index) => (
+                  <Grid item key={index} xs={12} sm={6} md={3}>
+                    <Slide direction="up" in timeout={300}>
+                      <Link to="/dormitory" style={{ textDecoration: 'none' }}>
+                        <Card
+                          style={{
+                            backgroundColor: '#f3f3f3',
+                            boxShadow: '0px 0px 10px rgba(148, 9, 9, 0.479)',
+                            borderRadius: '10px',
+                            transition: 'width 250ms ease-in-out, transform 150ms ease',
+                            '&:hover': {
+                              cursor: 'pointer',
+                            },
+                          }}
+                        >
+                          <Grow in timeout={300}>
+                            <CardContent>
+                              <Container
+                                style={{
+                                  backgroundColor: '#bf1a2f',
+                                  padding: '8px',
+                                  marginBottom: '8px',
+                                  borderRadius: '10px',
+                                }}
+                              >
+                                <Typography variant="h6" gutterBottom style={{ fontWeight: 'bold', color: '#ffffff' }}>
+                                  Konvikti {dormitory.dormNo}
+                                </Typography>
+                              </Container>
+                              <Typography variant="body2" color="textSecondary">
+                                Gjinia: {dormitory.gender}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary">
+                                Numri i dhomave: {dormitory.noOfRooms}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary">
+                                Kapaciteti: {dormitory.capacity}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary">
+                                Studentët aktual: {dormitory.currentStudents}
+                              </Typography>
+                            </CardContent>
+                          </Grow>
+                        </Card>
+                      </Link>
+                    </Slide>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </Grid>
+        </Grid>
+      </div>
       <ToastContainer />
     </div>
   );
