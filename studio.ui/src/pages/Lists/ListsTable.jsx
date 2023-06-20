56722ba2eb0f9a9586da6bbb3d465f08083c4214
almanovoberdaliu <93130020+almanovoberdaliu@users.noreply.github.com>
2023-06-21 00:13:02 +0200

    fixed list

--- studio.ui/src/pages/Lists/ListsTable.jsx
+++ studio.ui/src/pages/Lists/ListsTable.jsx
@@ -1,32 +1,36 @@
 import { Box } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 
-const ListsTable = (props) => {
-  const { profileMatches } = props;
-  const columns = [
-    { field: 'studentId', headerName: 'Student Id' },
-    { field: 'pointsForGPA', headerName: 'Piket nga nota mesatare' },
-    { field: 'extraPoints', headerName: 'Piket ekstra' },
-    { field: 'totalPoints', headerName: 'Piket ne total' },
-  ];
+const ListsTable = (props) =>
+{
+    const { profileMatches } = props;
+    const columns = [
+        { field: 'id', headerName: 'Id' },
+        { field: 'firstName', headerName: 'First Name' },
+        { field: 'lastName', headerName: 'Last Name' },
+        { field: 'email', headerName: 'Email' },
+        { field: 'city', headerName: 'City' },
+        { field: 'pointsForGPA', headerName: 'Average Grade Points' },
+        { field: 'major', headerName: 'Major' },
+    ];
 
-  return (
-    <Box marginTop={'32px'} width={'100%'}>
-      <div className="resource-table-container">
-        <DataGrid
-          rows={profileMatches}
-          columns={columns}
-          getRowId={(row) => row.id}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 10 },
-            },
-          }}
-          pageSizeOptions={[10, 15]}
-        />
-      </div>
-    </Box>
-  );
+    return (
+        <Box marginTop={'32px'} width={'100%'}>
+            <div className="resource-table-container">
+                <DataGrid
+                    rows={profileMatches}
+                    columns={columns}
+                    getRowId={(row) => row.id}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 10 },
+                        },
+                    }}
+                    pageSizeOptions={[10, 15]}
+                />
+            </div>
+        </Box>
+    );
 };
 
 export default ListsTable;
