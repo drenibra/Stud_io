85aaaf2ada8e0ec404929b3ff363ce917ccafce7
Fat Sijarina <fs51701@ubt-uni.net>
2023-06-20 20:37:05 +0200

    Fixed some small logical errors

--- studio.ui/src/pages/Lists/ListsTable.jsx
+++ studio.ui/src/pages/Lists/ListsTable.jsx
@@ -1,32 +1,30 @@
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid } from '@mui/x-data-grid';
 
+const ListsTable = (props) => {
+  const { profileMatches } = props;
+  const columns = [
+    { field: 'studentId', headerName: 'Student Id' },
+    //   { field: "city", headerName: "Qyteti" },
+    { field: 'pointsForGPA', headerName: 'Piket nga nota mesatare' },
+    { field: 'extraPoints', headerName: 'Piket ekstra' },
+    { field: 'totalPoints', headerName: 'Piket ne total' },
+  ];
 
-const ListsTable = (props) =>
-{
-
-    const { profileMatches } = props;
-    const columns = [
-        { field: "studentId", headerName: "Student Id" },
-        //   { field: "city", headerName: "Qyteti" },
-        { field: "pointsForGPA", headerName: "Piket nga nota mesatare" },
-        { field: "extraPoints", headerName: "Piket ekstra" },
-        { field: "totalPoints", headerName: "Piket ne total" }
-    ];
-
-    return (
-        <div className="lists-table">
-            <DataGrid
-                rows={profileMatches}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 10 },
-                    },
-                }}
-                pageSizeOptions={[10, 15]}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="lists-table">
+      <DataGrid
+        rows={profileMatches}
+        columns={columns}
+        getRowId={(row) => row.id}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 10 },
+          },
+        }}
+        pageSizeOptions={[10, 15]}
+      />
+    </div>
+  );
+};
 
 export default ListsTable;
